a80c2c7646051e73ea2e42b1a87ea82338d79bf0
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2025-02-05 09:36:49 -0500

    added require in the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
-import { resolve } from "path";
+const { resolve } = require("path");
 
-export const entry = "./leadmanager/frontend/src/index.js";
-export const output = {
-  path: resolve(__dirname, "leadmanager/frontend/static/frontend"),
-  filename: "main.js", // Output file
-};
-export const mode = "development";
-export const module = {
-  rules: [
-    {
-      test: /\.js$/, // Regex to target .js files
-      exclude: /node_modules/, // Exclude node_modules
-      use: {
-        loader: "babel-loader", // Use babel-loader
+module.exports = {
+  entry: "./leadmanager/frontend/src/index.js",
+  output: {
+    path: resolve(__dirname, "leadmanager/frontend/static/frontend"),
+    filename: "main.js", // Output file
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Regex to target .js files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: "babel-loader", // Use babel-loader
+        },
       },
-    },
-  ],
+    ],
+  },
 };
